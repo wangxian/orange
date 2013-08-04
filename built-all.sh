ac9c59066ee4c1fbc15e79c34d96497f914dc827
wangxian <xian366@126.com>
2013-08-05 00:43:44 +0800

    build-all

--- built-all.sh
+++ built-all.sh
@@ -8,6 +8,10 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build .
 mkdir -p bin/linux
 mv orange bin/linux/orange
 
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build .
+mkdir -p bin/win/
+mv orange.exe bin/win/orange-386.exe
+
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build .
 mkdir -p bin/win/
-mv orange.exe bin/win/orange.exe
+mv orange.exe bin/win/orange-64.exe
